ee51821eeffccddfe9327f300bc53586fcf49bb2
BanIkai <chinchisanupong1@gmail.com>
2026-05-14 11:27:23 +0700

    Update Config.h

--- Config.h
+++ Config.h
@@ -1,39 +1,6 @@
 #pragma once
-
-// =========================
-// MOTOR
-// =========================
-
-#define PIN_L1 11
-#define PIN_L2 10
-
-#define PIN_R1 5
-#define PIN_R2 6
-
-
-
-// =========================
-// LINE SENSOR
-// =========================
-
-#define PIN_LINE_L A2
-#define PIN_LINE_R A7
-
-#define LINE_L_TH 500
-#define LINE_R_TH 500
-
-
-
-// =========================
-// XSHUT
-// =========================
-
-#define PIN_XSHUT_SL 13
-#define PIN_XSHUT_FL 12
-#define PIN_XSHUT_FC 4
 #define PIN_XSHUT_FR 2
 #define PIN_XSHUT_SR 3
-
 
 
 // =========================
@@ -47,31 +14,30 @@
 #define ADDR_SR 0x34
 
 
-
 // =========================
-// DISTANCE
+// DISTANCE (mm)
 // =========================
 
-#define RAM_DIST      200
-#define TRACK_DIST    450
-#define SIDE_DIST     350
-
+#define RAM_DIST       180
+#define TRACK_DIST     350
+#define SIDE_DIST      250
 
 
 // =========================
 // SPEED
 // =========================
 
-#define RAM_SPEED      255
+#define MAX_SPEED      255
 
-#define TRACK_FAST     255
-#define TRACK_SLOW     140
+#define ATTACK_SPEED   255
+#define TRACK_FAST     220
+#define TRACK_SLOW     130
 
-#define SEARCH_SPEED   180
+#define SEARCH_SPEED   170
+#define SEARCH_FAST    220
 
 #define ESCAPE_BACK    255
-#define ESCAPE_TURN    180
-
+#define ESCAPE_TURN    200
 
 
 // =========================
@@ -81,10 +47,9 @@
 #define ESCAPE_BACK_MS 180
 #define ESCAPE_TURN_MS 220
 
-#define LOCK_TIME      300
+#define LOCK_TIME      350
 
-#define SEARCH_TIME    600
-
+#define SEARCH_SWAP_MS 700
 
 
 // =========================
